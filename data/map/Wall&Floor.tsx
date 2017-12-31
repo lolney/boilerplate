973691b4501b6f4a821383a838c4b2cfccbb40c5
Luke Olney <lukeolney@gmail.com>
2017-12-30 21:17:29 -0800

    Add tutorial files

---
+++ data/map/Wall&Floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Wall&amp;Floor" tilewidth="32" tileheight="32" tilecount="216" columns="18">
+ <image source="../Downloads/New Project (2).png" width="600" height="400"/>
+</tileset>
---
+++ data/map/background_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="New Project" tilewidth="32" tileheight="32" tilecount="216" columns="18">
+ <image source="../Downloads/New Project.png" width="600" height="400"/>
+</tileset>
